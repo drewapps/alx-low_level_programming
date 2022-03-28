95f513c7163624f854cfc5840db0efde641a476b
drewapps <druapps@gmail.com>
2022-03-28 12:27:00 -0700

    0-memset.c file added

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,20 @@
 #include "main.h"
+
 /**
- *_memset -   fills memory with a constant byte
- *@s: pointer block of memory to fill
- *@b: value to set
- *@n: bytes of the memory
- *Return: dest
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
  */
 
 char *_memset(char *s, char b, unsigned int n)
-
 {
-README.md unsigned int i;
+unsigned int i;
 
-README.md /*Delacring FOR*/
-README.md for (i = 0; i < n; i++)
-README.md {
-README.md README.md *(s + i) = b; /*add 1 position s*/
-
-README.md } /*END FOR*/
-
-README.md return (s);
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
 }
