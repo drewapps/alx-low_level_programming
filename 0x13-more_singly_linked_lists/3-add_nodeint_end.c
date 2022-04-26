43cfcac7f77f50865baa754e4f01c61049367ef0
drewapps <druapps@gmail.com>
2022-04-26 06:36:33 -0700

    3-add_nodeint_end.c file added

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,40 +1,37 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_nodeint_end - add a new node at the end
- * of a linked list
- * @head: head of a list.
- * @n: n element.
- *
- * Return: address of the new element. NUll if it failed.
+ * add_nodeint_end - Add a new node at the end of a listint_t
+ * @n: The element of the node to be added
+ * @head: The head/first node of the list
+ * Return: The address of the new element, NULL if failed
  */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *new;
-listint_t *temp;
+listint_t *new_head;
+listint_t *lastnode;
 
-(void)temp;
-
-new = malloc(sizeof(listint_t));
-
-if (new == NULL)
+new_head = malloc(sizeof(listint_t));
+if (new_head == NULL)
 return (NULL);
 
-new->n = n;
-new->next = NULL;
-temp = *head;
+new_head->n = n;
+new_head->next = NULL;
+
 if (*head == NULL)
 {
-*head = new;
-}
-else
-{
-while (temp->next != NULL)
-{
-temp = temp->next;
-}
-temp->next = new;
+*head = new_head;
+return (new_head);
 }
 
-return (*head);
+else
+{
+lastnode = *head;
+while (lastnode->next)
+lastnode = lastnode->next;
+lastnode->next = new_head;
+}
+return (new_head);
 }
