e63cf7159d20c47a9f5332ccb17935e93b314e1d
drewapps <druapps@gmail.com>
2022-06-16 17:57:19 +0300

    0x12-singly_linked_lists directory upated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,28 @@
 #include "lists.h"
+#include <stdio.h>
+
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
 {
-README.md size_t nelem;
+	size_t nodes = 0;
 
-README.md nelem = 0;
-README.md while (h != NULL)
-README.md {
-README.md README.md if (h->str == NULL)
-README.md README.md README.md printf("[%d] %s\n", 0, "(nil)");
-README.md README.md else
-README.md README.md README.md printf("[%d] %s\n", h->len, h->str);
-README.md README.md h = h->next;
-README.md README.md nelem++;
-README.md }
-README.md return (nelem);
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
